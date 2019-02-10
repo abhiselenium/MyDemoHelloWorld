7b5cb4db9589f5b41cd4ad3836b1a4fe88af4c8d
Abhi007 <Abhi007@God>
2019-02-10 12:20:38 -0800

    commit 2

---
+++ HelloworldProject/src/main/java/com/hello/p1/Practice1.java
@@ -0,0 +1,10 @@
+package com.hello.p1;
+
+public class Practice1 {
+
+	public static void display() {
+		System.out.println("hari om");
+
+	}
+
+}
--- HelloworldProject/src/main/java/com/hello/p1/prog1.java
+++ HelloworldProject/src/main/java/com/hello/p1/prog1.java
@@ -1,12 +1,12 @@
 package com.hello.p1;
 
-public class p1 
+public class prog1
 {
 
 	public static void main(String[] args) 
 	{
 		System.out.println(" Helloworld ");
-
+		Practice1.display();
 	}
 
 }
